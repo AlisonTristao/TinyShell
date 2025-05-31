d4a6702bf9dbdb2fe588ac5bd98c455b03420aae
AlisonTristao <AlisonTristao@hotmail.com>
2025-05-31 18:15:46 -0300

    vector to array

--- TableLinker/TableLinker.h
+++ TableLinker/TableLinker.h
@@ -1,7 +1,6 @@
 #ifndef TABLE_LINKER_H
 #define TABLE_LINKER_H
 
-#include <vector>
 #include <memory>
 #include <functional>
 #include <typeindex>
@@ -10,69 +9,120 @@
 
 using namespace std;
 
+// ****************************************
+// * templates to converte typeid to char *
+// ****************************************
+
+template<typename T>
+constexpr char char_type() {
+    if      constexpr (std::is_same<T, int>::value)     return 'i';
+    else if constexpr (std::is_same<T, float>::value)   return 'f';
+    else if constexpr (std::is_same<T, double>::value)  return 'd';
+    else if constexpr (std::is_same<T, char>::value)    return 'c';
+    else if constexpr (std::is_same<T, String>::value)  return 'S';
+    else                                                return '?';  // UNKNOWN
+}
+
 // *************************************
 // * class to create generic functions *
 // *************************************
 
 // abstract class to create the generics
 class base_function {
-public:
-    virtual ~base_function() {}
-    virtual uint8_t call(void** args) = 0;
-    virtual const vector<type_index>& getParamTypes() const = 0;
+    public:
+        virtual ~base_function() {
+            delete[] param_types;
+        }
+        virtual uint8_t call(void** args) = 0;
+        virtual const char* get_param_types() const = 0;
+        virtual size_t get_size() const = 0;
+    protected:
+        char* param_types;
+        size_t size;
 };
 
 // template class to save functions with variable parameters
-template<typename... params>
+template<typename... param>
 class class_function : public base_function {
-    function<uint8_t(params...)> func;
-    vector<type_index> paramTypes;
+    public:
+        class_function(function<uint8_t(param...)> f) : func(f) {
+            size = sizeof...(param);
+            param_types = new char[size];
+            
+            // convert tipe_id to char - gnu++ 17
+            size_t i = 0;
+            using expander = int[];
+            (void)expander{0, ((param_types[i++] = char_type<param>()), 0)...};
+        }
 
-public:
-    class_function(function<uint8_t(params...)> f) : func(f) {
-        paramTypes = { type_index(typeid(params))... };
-    }
+        uint8_t call(void** args) override {
+            return callImpl(args, index_sequence_for<param...>{});
+        }
 
-    uint8_t call(void** args) override {
-        return callImpl(args, index_sequence_for<params...>{});
-    }
+        const char* get_param_types() const override {
+            return param_types;
+        }
 
-    const vector<type_index>& getParamTypes() const override {
-        return paramTypes;
-    }
+        size_t get_size() const override {
+            return size;
+        }
 
-private:
-    template<size_t... Is>
-    uint8_t callImpl(void** args, index_sequence<Is...>) {
-        return func(*reinterpret_cast<typename remove_reference<params>::type*>(args[Is])...);
-    }
+    private:
+        template<size_t... Is>
+        uint8_t callImpl(void** args, index_sequence<Is...>) {
+            return func(*reinterpret_cast<typename remove_reference<param>::type*>(args[Is])...);
+        }
+        function<uint8_t(param...)> func;
 };
 
 // class to save the pointers
 class function_manager {
     // limit to functions pointers
-    uint8_t limit = 255;
-    vector<unique_ptr<base_function>> func_vector;
+    unique_ptr<base_function>* func_array;
+    size_t size;
+
     public:
-        template<typename... params>
-        void add(uint8_t(*f)(params...)) {
+        function_manager(size_t s) : size(s) {
+            func_array = new unique_ptr<base_function>[size];
+        }
+
+        template<typename... param>
+        uint8_t add(uint8_t idx, uint8_t(*f)(param...)) {
             // check vector limit
-            if (func_vector.size() == limit) return 253;
+            if (check_index(idx)) return 255;
 
             // add into vector
-            func_vector.push_back(make_unique<class_function<params...>>(function<uint8_t(params...)>(f)));
+            func_array[idx] = make_unique<class_function<param...>>(function<uint8_t(param...)>(f));
+            return 0;
         }
 
-        uint8_t call(int index, void** args) {
+        uint8_t call(int idx, void** args) {
             // check the intem index
-            if (index < 0 || index >= (int)func_vector.size()) return 255;
+            if (check_index(idx)) return 255;
 
             // call the function and return the result
-            return func_vector[index]->call(args);
+            return func_array[idx]->call(args);
         }
 
-        const vector<type_index>& getParamTypes(int index) const {
-            return func_vector[index]->getParamTypes();
+        const char* get_param_types(uint8_t idx) const {
+            return func_array[idx]->get_param_types();
+        }
+
+        size_t get_param_sizes(uint8_t idx) const {
+            return func_array[idx]->get_size();
+        }
+
+        String get_expected_types_string(uint8_t idx){
+            String text = "";
+            const char* type_parameters = get_param_types(idx);
+            for(uint8_t i; i < get_param_sizes(0); i++)
+                text += type_parameters[i];
+            return text;
+        }
+
+    private:
+        bool check_index(uint8_t index) {
+            return (index < 0 || index > size);
         }
 };
 
